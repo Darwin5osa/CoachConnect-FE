2e29aec3ec91ac66364e9da438b82dc40c03492a
lauberna <laureanoberna2002@gmail.com>
2024-02-22 13:35:50 -0300

    correcciones

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Servicios from "./Paginas/Servicios.jsx";
 import Contacto from "./Paginas/Contacto.jsx";
 import Register from "./Paginas/Register.jsx";
 import Acerca from "./Paginas/Acerca.jsx";
@@ -14,14 +13,12 @@ import Detail from "./Paginas/Detail.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   {/*  <App/> */}
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/" element={<Initial />} />
           <Route path="/acerca-de" element={<Acerca />} />
           <Route path="/contacto" element={<Contacto />} />
-          <Route path="/mentores" element={<Servicios />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registrarse" element={<Register />} />
           <Route path="/detalle/:id" element={<Detail />} />
