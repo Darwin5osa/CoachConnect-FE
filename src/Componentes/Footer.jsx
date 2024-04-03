0f137a2e410d706b75646c34a44961b66920588c
Agustina Zabaleta <agustina.zabaleta@oracle.com>
2024-04-03 15:40:19 -0300

    subiendo tarea #130

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,5 +1,9 @@
 import s from "./css/footer.module.css";
 import React from "react";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FiInstagram } from "react-icons/fi";
+import { BsDiscord } from "react-icons/bs";
+import { BsTwitterX  } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -7,7 +11,21 @@ const Footer = () => {
       <footer className={s.footer}>
         <div className={s.left}>
           <img className={s.logo} src="./Asset1.png" alt="logo" />
+          
           <p className={s.copy}>© 2024 - COACH CONNECT</p>
+          <a href="https://wa.me/34624366585" target="_blank" rel="noopener noreferrer">
+        <IoLogoWhatsapp size={30} color="#4BC8AB" />
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <FiInstagram  size={30} color="#4BC8AB" />
+      </a>
+      <a href="https://discord.gg/uWZgGWtg" target="_blank" rel="noopener noreferrer">
+        <BsDiscord  size={30} color="#4BC8AB" />
+      </a>
+      <a href="https://twitter.com/i/flow/login" target="_blank" rel="noopener noreferrer">
+        <BsTwitterX   size={30} color="#4BC8AB" />
+      </a>
+      
         </div>
         <div className={s.line}></div>
         <a
