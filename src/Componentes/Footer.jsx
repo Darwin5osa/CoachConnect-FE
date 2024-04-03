77368b206c447a426b81c1f8f70f0374938343d7
Agustina Zabaleta <agustina.zabaleta@oracle.com>
2024-04-03 18:40:14 -0300

    subiendo feature 131 footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -16,19 +16,34 @@ const Footer = () => {
           <img className={s.logo} src="/Asset1.png" alt="logo" />
           <p className={s.copy}>© 2024 - COACH CONNECT</p>
         </div>
-        <a href="https://wa.me/34624366585" target="_blank" rel="noopener noreferrer">
-        <IoLogoWhatsapp size={30} color="#4BC8AB" />
-      </a>
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-        <FiInstagram  size={30} color="#4BC8AB" />
-      </a>
-      <a href="https://discord.gg/uWZgGWtg" target="_blank" rel="noopener noreferrer">
-        <BsDiscord  size={30} color="#4BC8AB" />
-      </a>
-      <a href="https://twitter.com/i/flow/login" target="_blank" rel="noopener noreferrer">
-        <RiTwitterXFill   size={30} color="#4BC8AB" />
-      </a>
-      
+        <a
+          href="https://wa.me/34624366585"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <IoLogoWhatsapp size={30} color="#4BC8AB" />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FiInstagram size={30} color="#4BC8AB" />
+        </a>
+        <a
+          href="https://discord.gg/uWZgGWtg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsDiscord size={30} color="#4BC8AB" />
+        </a>
+        <a
+          href="https://twitter.com/i/flow/login"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <RiTwitterXFill size={30} color="#4BC8AB" />
+        </a>
         <a
           className={s.email}
           title="Email"
