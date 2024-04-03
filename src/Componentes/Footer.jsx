2df74da1f96d65cde65f8b5d9dd2b0a5389dfd05
Agustina Zabaleta <agustina.zabaleta@oracle.com>
2024-04-03 18:29:11 -0300

    subiendo feature 130 version actualizada

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -2,7 +2,10 @@ import { useLocation } from "react-router-dom";
 
 import s from "./css/footer.module.css";
 import React from "react";
-
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FiInstagram } from "react-icons/fi";
+import { BsDiscord } from "react-icons/bs";
+import { RiTwitterXFill } from "react-icons/ri";
 const Footer = () => {
   const location = useLocation();
 
@@ -13,7 +16,19 @@ const Footer = () => {
           <img className={s.logo} src="/Asset1.png" alt="logo" />
           <p className={s.copy}>© 2024 - COACH CONNECT</p>
         </div>
-        <div className={s.line}></div>
+        <a href="https://wa.me/34624366585" target="_blank" rel="noopener noreferrer">
+        <IoLogoWhatsapp size={30} color="#4BC8AB" />
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <FiInstagram  size={30} color="#4BC8AB" />
+      </a>
+      <a href="https://discord.gg/uWZgGWtg" target="_blank" rel="noopener noreferrer">
+        <BsDiscord  size={30} color="#4BC8AB" />
+      </a>
+      <a href="https://twitter.com/i/flow/login" target="_blank" rel="noopener noreferrer">
+        <RiTwitterXFill   size={30} color="#4BC8AB" />
+      </a>
+      
         <a
           className={s.email}
           title="Email"
