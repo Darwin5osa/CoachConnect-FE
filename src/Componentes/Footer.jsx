b0028d55b26f982c21216ec6a148aabcac6d11e3
Agustina Zabaleta <agustina.zabaleta@oracle.com>
2024-04-03 15:57:06 -0300

    footer pusheado

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FiInstagram } from "react-icons/fi";
 import { BsDiscord } from "react-icons/bs";
-import { BsTwitterX  } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
@@ -11,8 +11,9 @@ const Footer = () => {
       <footer className={s.footer}>
         <div className={s.left}>
           <img className={s.logo} src="./Asset1.png" alt="logo" />
-          
           <p className={s.copy}>© 2024 - COACH CONNECT</p>
+          </div>
+          
           <a href="https://wa.me/34624366585" target="_blank" rel="noopener noreferrer">
         <IoLogoWhatsapp size={30} color="#4BC8AB" />
       </a>
@@ -23,11 +24,9 @@ const Footer = () => {
         <BsDiscord  size={30} color="#4BC8AB" />
       </a>
       <a href="https://twitter.com/i/flow/login" target="_blank" rel="noopener noreferrer">
-        <BsTwitterX   size={30} color="#4BC8AB" />
+        <RiTwitterXFill   size={30} color="#4BC8AB" />
       </a>
       
-        </div>
-        <div className={s.line}></div>
         <a
           className={s.email}
           title="Email"
