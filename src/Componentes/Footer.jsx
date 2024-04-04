d2b71c116a6806dbce62f55701de7b9f036fd0cb
lauberna <laureanoberna2002@gmail.com>
2024-04-04 00:45:34 -0300

    corrigiendo links footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,11 +1,11 @@
 import { useLocation } from "react-router-dom";
 
-import s from "./css/footer.module.css";
-import React from "react";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { RiTwitterXFill } from "react-icons/ri";
 import { FiInstagram } from "react-icons/fi";
 import { BsDiscord } from "react-icons/bs";
-import { RiTwitterXFill } from "react-icons/ri";
+import s from "./css/footer.module.css";
+import React from "react";
 const Footer = () => {
   const location = useLocation();
 
@@ -16,34 +16,36 @@ const Footer = () => {
           <img className={s.logo} src="/Asset1.png" alt="logo" />
           <p className={s.copy}>© 2024 - COACH CONNECT</p>
         </div>
-        <a
-          href="https://wa.me/34624366585"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <IoLogoWhatsapp size={30} color="#4BC8AB" />
-        </a>
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FiInstagram size={30} color="#4BC8AB" />
-        </a>
-        <a
-          href="https://discord.gg/uWZgGWtg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <BsDiscord size={30} color="#4BC8AB" />
-        </a>
-        <a
-          href="https://twitter.com/i/flow/login"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <RiTwitterXFill size={30} color="#4BC8AB" />
-        </a>
+        <div className={s.links}>
+          <a
+            href="https://wa.me/34624366585"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoWhatsapp className={s.ico} />
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiInstagram className={s.ico} />
+          </a>
+          <a
+            href="https://discord.gg/uWZgGWtg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsDiscord className={s.ico} />
+          </a>
+          <a
+            href="https://twitter.com/i/flow/login"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <RiTwitterXFill className={s.ico} />
+          </a>
+        </div>
         <a
           className={s.email}
           title="Email"
